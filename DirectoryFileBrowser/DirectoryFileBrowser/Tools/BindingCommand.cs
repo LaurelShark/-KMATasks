02d68f283d43f493d44b6072bc38f3c6175f0ba3
Andrii Lysenko <senkoily@gmail.com>
2018-11-07 22:27:12 +0200

    add BindingCommand.cs, bindings

---
+++ DirectoryFileBrowser/DirectoryFileBrowser/Tools/BindingCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DirectoryFileBrowser.Tools
+{
+    public class BindingCommand<T> : ICommand
+    {
+        #region Fields
+        readonly Action<T> _execute;
+        readonly Predicate<T> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+        #endregion
+
+        public BindingCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute != null)
+                _execute = execute;
+            else
+                throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
