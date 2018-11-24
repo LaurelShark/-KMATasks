3ca5ff0c382d312be835c41e5b1463e34404b26c
Yaroslav Kladko <yarikhaker@gmail.com>
2018-11-24 23:26:45 +0200

    wcf service created

---
+++ DirectoryFileBrowser/AppService/DFBWindowsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryFileBrowser
+{
+    class DFBWindowsService : ServiceBase
+    {
+        #region Const
+        internal const string CurrentServiceName = "DFBService";
+        #endregion
+    }
+}
